3db7ff42a743d80b28ded766425a155b4e047811
ywanzhou <ywanzhou@foxmail.com>
2022-11-08 14:33:27 +0800

    chore: 🔨 install eslint-plugin-tailwindcss

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
 <template>
-  <div>
-    <h1 class="text-red-400 text-2xl">
-      hello world
-    </h1>
+  <div class="min-h-screen hero" :style="{ backgroundImage: 'url(https://placeimg.com/1000/800/arch)' }">
+    <div class="bg-opacity-60 hero-overlay" />
+    <div class="text-center hero-content text-neutral-content">
+      <div class="max-w-md">
+        <h1 class="mb-5 text-5xl font-bold">
+          Hello there
+        </h1>
+        <p class="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+        </p>
+        <button class="btn btn-primary">
+          Get Started
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
