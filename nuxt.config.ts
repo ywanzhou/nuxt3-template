48ef40ba4aa95c53635506aee472f226eb8ee3f2
ywanzhou <ywanzhou@foxmail.com>
2022-11-09 14:16:25 +0800

    feat: 🚀 配置pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
 })
